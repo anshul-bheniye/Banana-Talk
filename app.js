74c882f6777b42102f7a4bf2df5acc37104af0e7
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2021-04-11 23:20:29 +0530

    variable set and import api

---
+++ app.js
@@ -0,0 +1,5 @@
+var btnClick = document.querySelector("#tapme");
+var txt = document.querySelector("#textArea");
+var div = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
